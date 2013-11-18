013c9a4421df7f08f25466ab9afc9efca4769792
andylstuy <andyl.stuy@gmail.com>
2013-11-18 14:05:24 -0500

    Update Rational.java

--- Rational.java
+++ Rational.java
@@ -1,4 +1,6 @@
 public class Rational {
+  
+  70:9f:ff:1f:00:9d:57:77:9e:57:03:8a:02:55:53:8d
 
 
 
@@ -19,7 +21,14 @@ public class Rational {
 
 
 
+private static int numerator;
+    private static int denominator;
 
+    public Rational() {
+	numerator = 0;
+	denominator = 1;
+	Rational start = new Rational(numerator, denominator);
+    }
 
 
 
